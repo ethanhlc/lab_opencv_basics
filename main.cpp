8d86de74390fcabe2c33fd8dfb749d9ddf1006e1
Ethan Baik <sakithree@gmail.com>
2023-01-30 16:50:27 +0900

    Use namespace std, cv

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
 
+using namespace std;
+using namespace cv;
+
 int main()
 {
-    std::cout << "Hello OpenCV " << CV_VERSION << std::endl;
+    cout << "Hello OpenCV " << CV_VERSION << endl;
 
     return 0;
 }
