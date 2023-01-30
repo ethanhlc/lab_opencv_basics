580e54a7014fea3299c1f928456c1e2dc4377b75
Ethan Baik <sakithree@gmail.com>
2023-01-30 17:07:18 +0900

    Display img on screen
    Add image files to gitignore

--- main.cpp
+++ main.cpp
@@ -8,5 +8,18 @@ int main()
 {
     cout << "Hello OpenCV " << CV_VERSION << endl;
 
+    Mat img;
+    img = imread("lenna.bmp");
+
+    if (img.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return 1;
+    }
+
+    namedWindow("image");
+    imshow("image", img);
+
+    waitKey(0);
     return 0;
 }
