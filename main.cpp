46b908cea28913223de08f435847f20572013bf2
Ethan Baik <sakithree@gmail.com>
2023-02-01 14:54:16 +0900

    Point_ class test

--- main.cpp
+++ main.cpp
@@ -4,27 +4,51 @@
 using namespace std;
 using namespace cv;
 
+//// Function Declarations ////
+void PointExercise(void);
+
 int main()
 {
-    cout << "Hello OpenCV " << CV_VERSION << endl;
+    //cout << "Hello OpenCV " << CV_VERSION << endl;
 
-    Mat img;
-    img = imread("lenna.bmp", IMREAD_GRAYSCALE);
+    //Mat img;
+    //img = imread("lenna.bmp", IMREAD_GRAYSCALE);
 
-    if (img.empty())
-    {
-        cerr << "Image load failed!" << endl;
-        return 1;
-    }
+    //if (img.empty())
+    //{
+    //    cerr << "Image load failed!" << endl;
+    //    return 1;
+    //}
 
-    vector<int> params;
-    params.push_back(IMWRITE_JPEG_QUALITY);
-    params.push_back(95);
-    imwrite("lenna.jpg", img, params);
+    //vector<int> params;
+    //params.push_back(IMWRITE_JPEG_QUALITY);
+    //params.push_back(95);
+    //imwrite("lenna.jpg", img, params);
 
-    namedWindow("image");
-    imshow("image", img);
+    //namedWindow("image");
+    //imshow("image", img);
 
-    waitKey(0);
+    //waitKey(0);
+
+    PointExercise();
+
     return 0;
+}
+
+//// Function Definitions ////
+void PointExercise(void)
+{
+    Point pt1;
+    pt1.x = 15;
+    pt1.y = 20;
+
+    Point pt2(10, 30);
+
+    Point pt3 = pt1 + pt2;
+    Point pt4 = pt1 * 2;
+
+    cout << "pt1: " << pt1 << endl;
+    cout << "pt2: " << pt2 << endl;
+    cout << "pt3 = pt1 + pt2: " << pt3 << endl;
+    cout << "pt4 = pt1 * 2: " << pt4 << endl;
 }
