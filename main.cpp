2df6d7cc436162d3107e33b08236cb154ae29da7
Ethan Baik <sakithree@gmail.com>
2023-02-01 13:47:01 +0900

    Display image in grayscale & write to jpg file

--- main.cpp
+++ main.cpp
@@ -9,13 +9,18 @@ int main()
     cout << "Hello OpenCV " << CV_VERSION << endl;
 
     Mat img;
-    img = imread("lenna.bmp");
+    img = imread("lenna.bmp", IMREAD_GRAYSCALE);
 
     if (img.empty())
     {
         cerr << "Image load failed!" << endl;
         return 1;
     }
+
+    vector<int> params;
+    params.push_back(IMWRITE_JPEG_QUALITY);
+    params.push_back(95);
+    imwrite("lenna.jpg", img, params);
 
     namedWindow("image");
     imshow("image", img);
