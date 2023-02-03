9b7c71924a73f3292cb182ba5bd9f26905623409
Ethan Baik <sakithree@gmail.com>
2023-02-03 11:12:25 +0900

    MatOp4 exercise

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ void RotatedRectExercise(void);
 void MatOp1(void);
 void MatOp2(void);
 void MatOp3(void);
+void MatOp4(void);
 
 int main()
 {
@@ -21,7 +22,8 @@ int main()
     // RotatedRectExercise();
     // MatOp1();
     // MatOp2();
-    MatOp3();
+    // MatOp3();
+    MatOp4();
 
     return 0;
 }
@@ -182,4 +184,33 @@ void MatOp3(void)
 
     waitKey();
     destroyAllWindows();
+}
+
+void MatOp4(void)
+{
+    Mat mat1 = Mat::zeros(3, 4, CV_8UC1);
+
+    for (int j = 0; j < mat1.rows; j++)
+    {
+        for (int i = 0; i < mat1.cols; i++)
+        {
+            mat1.at<uchar>(j, i)++;
+        }
+    }
+
+    for (int j = 0; j < mat1.rows; j++)
+    {
+        uchar *p = mat1.ptr<uchar>(j);
+        for (int i = 0; i < mat1.cols; i++)
+        {
+            p[i]++;
+        }
+    }
+
+    for (MatIterator_<uchar> it = mat1.begin<uchar>(); it != mat1.end<uchar>(); it++)
+    {
+        (*it)++;
+    }
+
+    cout << "mat1:\n" << mat1 << endl;
 }
