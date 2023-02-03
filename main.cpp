f592f7de2f50360f033ee440483b9b97e20ecdfb
Ethan Baik <sakithree@gmail.com>
2023-02-03 09:42:33 +0900

    MatOp1 exercise

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace cv;
 void PointExercise(void);
 void RectExercise(void);
 void RotatedRectExercise(void);
+void MatOp1(void);
 
 int main()
 {
@@ -34,7 +35,8 @@ int main()
 
     //PointExercise();
     //RectExercise();
-    RotatedRectExercise();
+    //RotatedRectExercise();
+    MatOp1();
 
     return 0;
 }
@@ -96,4 +98,32 @@ void RotatedRectExercise(void)
     cout << "pts[0]: " << pts[0] << " pts[1]: " << pts[1] << endl << "pts[2]: " << pts[2] << " pts[3]: " << pts[3] << endl << endl;
     cout << "br: " << br << endl;
     cout << "br_float: " << br_f << endl;
+}
+
+void MatOp1(void)
+{
+    Mat img1;       // empty matrix
+
+    Mat img2(480, 640, CV_8UC1);        // unsigned char, 1-channel
+    Mat img3(480, 640, CV_8UC3);        // unsigned char, 3-channels
+    Mat img4(Size(640, 480), CV_8UC3);  // Size(width, height)
+
+    Mat img5(480, 640, CV_8UC1, Scalar(128));           // initial values, 128
+    Mat img6(480, 640, CV_8UC3, Scalar(0, 0, 255));     // initial values, red
+
+    Mat mat1 = Mat::zeros(3, 3, CV_32SC1);  // 0's matrix
+    Mat mat2 = Mat::ones(3, 3, CV_32FC1);   // 1's matrix
+    Mat mat3 = Mat::eye(3, 3, CV_32FC1);    // identity matrix
+
+    float data[] = { 1,2,3,4,5,6 };
+    Mat mat4(2, 3, CV_32FC1, data);
+
+    Mat mat5 = (Mat_<float>(2, 3) << 1, 2, 3, 4, 5, 6);
+    Mat mat6 = Mat_<uchar>({ 2, 3 }, { 1, 2, 3, 4, 5, 6 });
+
+    mat4.create(256, 256, CV_8UC3);
+    mat5.create(4, 4, CV_32FC1);
+
+    mat4 = Scalar(255, 0, 0);
+    mat5.setTo(1.f);
 }
