b0afc4260f05a1c9d0f3ce3150d2262387f8ec8e
Ethan Baik <sakithree@gmail.com>
2023-02-03 14:20:53 +0900

    MatOp7 exercise

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ void MatOp3(void);
 void MatOp4(void);
 void MatOp5(void);
 void MatOp6(void);
+void MatOp7(void);
 
 int main()
 {
@@ -27,7 +28,8 @@ int main()
     // MatOp3();
     // MatOp4();
     // MatOp5();
-    MatOp6();
+    // MatOp6();
+    MatOp7();
 
     return 0;
 }
@@ -250,4 +252,26 @@ void MatOp6(void)
     cout << "mat1 + 3:\n" << mat1 + 3 << endl;
     cout << "mat1 + mat2:\n" << mat1 + mat2 << endl;
     cout << "mat1 * mat2:\n" << mat1 * mat2 << endl;
+}
+
+void MatOp7(void)
+{
+    Mat img1 = imread("img/lenna.bmp", IMREAD_GRAYSCALE);
+
+    Mat img1f;
+    img1.convertTo(img1f, CV_32FC1);
+
+    uchar data1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    Mat mat1(3, 4, CV_8UC1, data1);
+    Mat mat2 = mat1.reshape(0, 1);
+
+    cout << "mat1:\n" << mat1 << endl;
+    cout << "mat2:\n" << mat2 << endl;
+
+    Mat mat3 = Mat::ones(1, 4, CV_8UC1) * 255;
+    mat1.push_back(mat3);
+    cout << "mat1:\n" << mat1 << endl;
+
+    mat1.resize(6, 100);
+    cout << "mat1:\n" << mat1 << endl;
 }
