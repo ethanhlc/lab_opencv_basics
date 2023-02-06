d6667b9028aa429ddac6040dacfdbea3c95f0e49
Ethan Baik <sakithree@gmail.com>
2023-02-06 16:26:48 +0900

    mouseEvent ex2
    Ctrl mouse - draw in red

--- main.cpp
+++ main.cpp
@@ -320,7 +320,13 @@ void on_mouse(int event, int x, int y, int flags, void *)
         break;
     case EVENT_MOUSEMOVE:
         // cout << "EVENT_MOUSEMOVE: " << x << ", " << y << endl;
-        if (flags & EVENT_FLAG_LBUTTON)
+        if ((flags & EVENT_FLAG_LBUTTON) && (flags & EVENT_FLAG_CTRLKEY))
+        {
+            line(img, ptOld, Point(x, y), Scalar(0, 0, 255), 2);
+            imshow("img", img);
+            ptOld = Point(x, y);
+        }
+        else if (flags & EVENT_FLAG_LBUTTON)
         {
             line(img, ptOld, Point(x, y), Scalar(0, 255, 255), 2);
             imshow("img", img);
