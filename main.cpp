f39315b839e16a4fb2de3c1023039f43f267a340
Ethan Baik <sakithree@gmail.com>
2023-02-06 14:56:52 +0900

    drawText2 ex

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ void camera_in_video_out();
 void drawLines();
 void drawPolys();
 void drawText1();
+void drawText2();
 
 int main()
 {
@@ -18,7 +19,8 @@ int main()
     // camera_in_video_out();
     // drawLines();
     // drawPolys();
-    drawText1();
+    // drawText1();
+    drawText2();
 
     return 0;
 }
@@ -202,15 +204,46 @@ void drawText1()
 {
     Mat img(500, 800, CV_8UC3, Scalar(255, 255, 255));
 
-    putText(img, "FONT_HERSHEY_SIMPLEX", Point(20, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255));
-    putText(img, "FONT_HERSHEY_PLAIN", Point(20, 100), FONT_HERSHEY_PLAIN, 1, Scalar(0, 0, 255));
-    putText(img, "FONT_HERSHEY_DUPLEX", Point(20, 150), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 255));
-    putText(img, "FONT_HERSHEY_COMPLEX", Point(20, 200), FONT_HERSHEY_COMPLEX, 1, Scalar(255, 0, 0));
-    putText(img, "FONT_HERSHEY_TRIPLEX", Point(20, 250), FONT_HERSHEY_TRIPLEX, 1, Scalar(255, 0, 0));
-    putText(img, "FONT_HERSHEY_COMPLEX_SMALL", Point(20, 300), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 0, 0));
-    putText(img, "FONT_HERSHEY_SCRIPT_SIMPLEX", Point(20, 350), FONT_HERSHEY_SCRIPT_SIMPLEX, 1, Scalar(255, 0, 255));
-    putText(img, "FONT_HERSHEY_SCRIPT_COMPLEX", Point(20, 400), FONT_HERSHEY_SCRIPT_COMPLEX, 1, Scalar(255, 0, 255));
-    putText(img, "FONT_HERSHEY_COMPLEX | FONT_ITALIC", Point(20, 450), FONT_HERSHEY_COMPLEX | FONT_ITALIC, 1, Scalar(255, 0, 0));
+    putText(img, "FONT_HERSHEY_SIMPLEX", Point(20, 50),
+            FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255));
+    putText(img, "FONT_HERSHEY_PLAIN", Point(20, 100),
+            FONT_HERSHEY_PLAIN, 1, Scalar(0, 0, 255));
+    putText(img, "FONT_HERSHEY_DUPLEX", Point(20, 150),
+            FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 255));
+    putText(img, "FONT_HERSHEY_COMPLEX", Point(20, 200),
+            FONT_HERSHEY_COMPLEX, 1, Scalar(255, 0, 0));
+    putText(img, "FONT_HERSHEY_TRIPLEX", Point(20, 250),
+            FONT_HERSHEY_TRIPLEX, 1, Scalar(255, 0, 0));
+    putText(img, "FONT_HERSHEY_COMPLEX_SMALL", Point(20, 300),
+            FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 0, 0));
+    putText(img, "FONT_HERSHEY_SCRIPT_SIMPLEX", Point(20, 350),
+            FONT_HERSHEY_SCRIPT_SIMPLEX, 1, Scalar(255, 0, 255));
+    putText(img, "FONT_HERSHEY_SCRIPT_COMPLEX", Point(20, 400),
+            FONT_HERSHEY_SCRIPT_COMPLEX, 1, Scalar(255, 0, 255));
+    putText(img, "FONT_HERSHEY_COMPLEX | FONT_ITALIC", Point(20, 450),
+            FONT_HERSHEY_COMPLEX | FONT_ITALIC, 1, Scalar(255, 0, 0));
+
+    imshow("img", img);
+    waitKey(0);
+
+    destroyAllWindows();
+}
+
+void drawText2()
+{
+    Mat img(200, 640, CV_8UC3, Scalar(255, 255, 255));
+
+    const String text = "Hello, OpenCV";
+    int fontFace = FONT_HERSHEY_TRIPLEX;
+    double fontScale = 2.0;
+    int thickness = 1;
+
+    Size sizeText = getTextSize(text, fontFace, fontScale, thickness, 0);
+    Size sizeImg = img.size();
+
+    Point org((sizeImg.width - sizeText.width) / 2, (sizeImg.height + sizeText.height) / 2);
+    putText(img, text, org, fontFace, fontScale, Scalar(255, 0, 0), thickness);
+    rectangle(img, org, org + Point(sizeText.width, -sizeText.height), Scalar(255, 0, 0), 1);
 
     imshow("img", img);
     waitKey(0);
