56330f585d6d620a551c2c54a488dc16f0710471
Ethan Baik <sakithree@gmail.com>
2023-02-06 10:30:48 +0900

    camera_in ex

--- main.cpp
+++ main.cpp
@@ -4,7 +4,43 @@
 using namespace cv;
 using namespace std;
 
+void camera_in();
+
 int main()
 {
+    camera_in();
+
     return 0;
+}
+
+void camera_in()
+{
+    VideoCapture cap(0);
+
+    if (!cap.isOpened())
+    {
+        cerr << "Camera open failed!" << endl;
+        return;
+    }
+
+    cout << "Frame width: " << cvRound(cap.get(CAP_PROP_FRAME_WIDTH)) << endl;
+    cout << "Frame height: " << cvRound(cap.get(CAP_PROP_FRAME_HEIGHT)) << endl;
+
+    Mat frame, inversed;
+    while (true)
+    {
+        cap >> frame;
+        if (frame.empty())
+            break;
+
+        inversed = ~frame;
+
+        imshow("frame", frame);
+        imshow("inversed", inversed);
+
+        if (waitKey(10) == 27)      // ESC key
+            break;
+    }
+
+    destroyAllWindows();
 }
