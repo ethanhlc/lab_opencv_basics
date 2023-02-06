be7d10781ec64eaf5d3bf104649b7e8aa87fa391
Ethan Baik <sakithree@gmail.com>
2023-02-06 14:39:01 +0900

    drawText1 ex

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ void video_in();
 void camera_in_video_out();
 void drawLines();
 void drawPolys();
+void drawText1();
 
 int main()
 {
@@ -16,7 +17,8 @@ int main()
     // video_in();
     // camera_in_video_out();
     // drawLines();
-    drawPolys();
+    // drawPolys();
+    drawText1();
 
     return 0;
 }
@@ -189,6 +191,26 @@ void drawPolys()
     pts.push_back(Point(250, 350));
     fillPoly(img, pts, Scalar(0, 255, 255));
     polylines(img, pts, true, Scalar(255, 0, 255), 2);
+
+    imshow("img", img);
+    waitKey(0);
+
+    destroyAllWindows();
+}
+
+void drawText1()
+{
+    Mat img(500, 800, CV_8UC3, Scalar(255, 255, 255));
+
+    putText(img, "FONT_HERSHEY_SIMPLEX", Point(20, 50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255));
+    putText(img, "FONT_HERSHEY_PLAIN", Point(20, 100), FONT_HERSHEY_PLAIN, 1, Scalar(0, 0, 255));
+    putText(img, "FONT_HERSHEY_DUPLEX", Point(20, 150), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 255));
+    putText(img, "FONT_HERSHEY_COMPLEX", Point(20, 200), FONT_HERSHEY_COMPLEX, 1, Scalar(255, 0, 0));
+    putText(img, "FONT_HERSHEY_TRIPLEX", Point(20, 250), FONT_HERSHEY_TRIPLEX, 1, Scalar(255, 0, 0));
+    putText(img, "FONT_HERSHEY_COMPLEX_SMALL", Point(20, 300), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255, 0, 0));
+    putText(img, "FONT_HERSHEY_SCRIPT_SIMPLEX", Point(20, 350), FONT_HERSHEY_SCRIPT_SIMPLEX, 1, Scalar(255, 0, 255));
+    putText(img, "FONT_HERSHEY_SCRIPT_COMPLEX", Point(20, 400), FONT_HERSHEY_SCRIPT_COMPLEX, 1, Scalar(255, 0, 255));
+    putText(img, "FONT_HERSHEY_COMPLEX | FONT_ITALIC", Point(20, 450), FONT_HERSHEY_COMPLEX | FONT_ITALIC, 1, Scalar(255, 0, 0));
 
     imshow("img", img);
     waitKey(0);
