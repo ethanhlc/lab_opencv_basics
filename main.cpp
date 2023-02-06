d578bf2f555042c989a6b05fb7fa7210aa26e08f
Ethan Baik <sakithree@gmail.com>
2023-02-06 14:03:06 +0900

    drawPolys ex

--- main.cpp
+++ main.cpp
@@ -8,13 +8,15 @@ void camera_in();
 void video_in();
 void camera_in_video_out();
 void drawLines();
+void drawPolys();
 
 int main()
 {
     // camera_in();
     // video_in();
     // camera_in_video_out();
-    drawLines();
+    // drawLines();
+    drawPolys();
 
     return 0;
 }
@@ -158,6 +160,35 @@ void drawLines()
     drawMarker(img, Point(250, 350), Scalar(0, 0, 255), MARKER_SQUARE);
     drawMarker(img, Point(300, 350), Scalar(0, 0, 255), MARKER_TRIANGLE_UP);
     drawMarker(img, Point(350, 350), Scalar(0, 0, 255), MARKER_TRIANGLE_DOWN);
+
+    imshow("img", img);
+    waitKey(0);
+
+    destroyAllWindows();
+}
+
+void drawPolys()
+{
+    Mat img(400, 400, CV_8UC3, Scalar(255, 255, 255));
+
+    rectangle(img, Rect(50, 50, 100, 50), Scalar(0, 0, 255), 2);
+    rectangle(img, Point(50, 150), Point(150, 200), Scalar(0, 0, 128), -1);
+
+    circle(img, Point(300, 120), 30, Scalar(255, 255, 0), -1, LINE_AA);
+    circle(img, Point(300, 120), 60, Scalar(255, 0, 0), 3, LINE_AA);
+
+    ellipse(img, Point(120, 300), Size(60, 30), 20, 0, 270, Scalar(255, 255, 0), -1, LINE_AA);
+    ellipse(img, Point(120, 300), Size(100, 50), 20, 0, 360, Scalar(0, 255, 0), 2, LINE_AA);
+
+    vector<Point> pts;
+    pts.push_back(Point(250, 250));
+    pts.push_back(Point(300, 250));
+    pts.push_back(Point(300, 300));
+    pts.push_back(Point(350, 300));
+    pts.push_back(Point(350, 350));
+    pts.push_back(Point(250, 350));
+    fillPoly(img, pts, Scalar(0, 255, 255));
+    polylines(img, pts, true, Scalar(255, 0, 255), 2);
 
     imshow("img", img);
     waitKey(0);
