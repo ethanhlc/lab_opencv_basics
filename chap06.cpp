28908eb806c0800abe8abffa7b15d3f0c86a0948
Ethan Baik <sakithree@gmail.com>
2023-02-13 10:20:51 +0900

    imgAdd exercise

--- chap06.cpp
+++ chap06.cpp
@@ -4,7 +4,28 @@
 using namespace std;
 using namespace cv;
 
+void imgAdd();
+
 int main(void)
 {
+    imgAdd();
+
     return 0;
+}
+
+void imgAdd()
+{
+    Mat src1 = imread("img/camera.bmp", IMREAD_GRAYSCALE);
+    Mat src2 = imread("img/dog.bmp", IMREAD_GRAYSCALE);
+
+    Mat dst1, dst2;
+
+    add(src1, src2, dst1);
+    addWeighted(src1, 0.5, src2, 0.5, 0, dst2);
+
+    imshow("dst1_simple", dst1);
+    imshow("dst2_weighted", dst2);
+
+    waitKey();
+    destroyAllWindows();
 }
