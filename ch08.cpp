f5067d96a80090597083875f0bb21d6667465369
Ethan Baik <sakithree@gmail.com>
2023-02-15 14:59:32 +0900

    Affine transform

--- ch08.cpp
+++ ch08.cpp
@@ -4,7 +4,45 @@
 using namespace std;
 using namespace cv;
 
+/// Function Declarations
+void affine_transform();
+
 int main(void)
 {
+    affine_transform();
+
     return 0;
+}
+
+/// Function Definitions
+void affine_transform()
+{
+    Mat src = imread("img/tekapo.bmp");
+
+    if (src.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return;
+    }
+
+    Point2f srcPts[3], dstPts[3];
+    srcPts[0] = Point2f(0, 0);
+    srcPts[1] = Point2f(src.cols - 1, 0);
+    srcPts[2] = Point2f(src.cols - 1, src.rows - 1);
+    dstPts[0] = Point2f(50, 50);
+    dstPts[1] = Point2f(src.cols - 100, 100);
+    dstPts[2] = Point2f(src.cols - 50, src.rows - 50);
+
+    // get transformation matrix from src/dst Points
+    Mat M = getAffineTransform(srcPts, dstPts);
+
+    // apply transformation to img
+    Mat dst;
+    warpAffine(src, dst, M, Size(0, 0), INTER_LINEAR);
+
+    imshow("src", src);
+    imshow("dst", dst);
+
+    waitKey();
+    destroyAllWindows();
 }
