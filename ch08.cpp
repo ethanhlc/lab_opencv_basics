b5bfee99aa2af630a8bb4afa5b1b240b97346c8b
Ethan Baik <sakithree@gmail.com>
2023-02-15 16:13:00 +0900

    shear transform

--- ch08.cpp
+++ ch08.cpp
@@ -7,11 +7,13 @@ using namespace cv;
 /// Function Declarations
 void affine_transform();
 void affine_translate();
+void affine_shear();
 
 int main(void)
 {
     // affine_transform();
-    affine_translate();
+    // affine_translate();
+    affine_shear();
 
     return 0;
 }
@@ -75,6 +77,34 @@ void affine_translate()
 
     imshow("src", src);
     imshow("dst", dst);
+
+    waitKey();
+    destroyAllWindows();
+}
+
+void affine_shear()
+{
+    Mat src = imread("img/tekapo.bmp");
+
+    if (src.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return;
+    }
+
+    double mx = 0.3;
+    Mat Mx = Mat_<double>({2,3}, {1, mx, 0, 0, 1, 0});
+
+    double my = 0.3;
+    Mat My = Mat_<double>({2,3}, {1, 0, 0, my, 1, 0});
+
+    Mat dst_x, dst_y;
+    warpAffine(src, dst_x, Mx, Size(src.cols + mx * src.rows, src.rows));
+    warpAffine(src, dst_y, My, Size(src.cols, src.rows + my * src.cols));
+
+    imshow("src", src);
+    imshow("dst_x", dst_x);
+    imshow("dst_y", dst_y);
 
     waitKey();
     destroyAllWindows();
