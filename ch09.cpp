bcac4aed0984cd849dedd8a938b7867e0082b885
Ethan Baik <sakithree@gmail.com>
2023-02-20 10:49:31 +0900

    sobel edge detection

--- ch09.cpp
+++ ch09.cpp
@@ -4,19 +4,41 @@
 using namespace std;
 using namespace cv;
 
+/// Function Declarations
+void sobel_edge();
+
 int main(void)
 {
-    Mat img = imread("img/lenna.bmp");
-
-    if (img.empty())
-    {
-        cout << "No img data!" << endl;
-        return -1;
-    }
-    imshow("img", img);
+    sobel_edge();
 
     waitKey();
     destroyAllWindows();
 
     return 0;
+}
+
+/// Function Definitions
+void sobel_edge()
+{
+    Mat src = imread("img/lenna.bmp", IMREAD_GRAYSCALE);
+
+    if (src.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return;
+    }
+
+    Mat dx, dy;
+    Sobel(src, dx, CV_32FC1, 1, 0);
+    Sobel(src, dy, CV_32FC1, 0, 1);
+
+    Mat fmag, mag;
+    magnitude(dx, dy, fmag);
+    fmag.convertTo(mag, CV_8UC1);
+
+    Mat edge = mag > 150;
+
+    imshow("src", src);
+    imshow("mag", mag);
+    imshow("edge", edge);
 }
