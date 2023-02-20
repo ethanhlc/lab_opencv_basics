821eaea1f1c6292202903c572ed5a520ebfaea3d
Ethan Baik <sakithree@gmail.com>
2023-02-20 15:01:48 +0900

    hough circles

--- ch09.cpp
+++ ch09.cpp
@@ -9,13 +9,15 @@ void sobel_edge();
 void canny_edge();
 void hough_lines();
 void hough_line_segments();
+void hough_circles();
 
 int main(void)
 {
     // sobel_edge();
     // canny_edge();
     // hough_lines();
-    hough_line_segments();
+    // hough_line_segments();
+    hough_circles();
 
     waitKey();
     destroyAllWindows();
@@ -125,6 +127,36 @@ void hough_line_segments()
     for (Vec4i l : lines)
     {
         line(dst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 2, LINE_AA);
+    }
+
+    imshow("src", src);
+    imshow("dst", dst);
+}
+
+void hough_circles()
+{
+    Mat src = imread("img/coins.png", IMREAD_GRAYSCALE);
+
+    if (src.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return;
+    }
+
+    Mat blurred;
+    blur(src, blurred, Size(3, 3));
+
+    vector<Vec3f> circles;
+    HoughCircles(blurred, circles, HOUGH_GRADIENT, 1, 50, 150, 30);
+
+    Mat dst;
+    cvtColor(src, dst, COLOR_GRAY2BGR);
+
+    for (Vec3f c : circles)
+    {
+        Point center(cvRound(c[0]), cvRound(c[1]));
+        int radius = cvRound(c[2]);
+        circle(dst, center, radius, Scalar(0, 0, 255), 2, LINE_AA);
     }
 
     imshow("src", src);
