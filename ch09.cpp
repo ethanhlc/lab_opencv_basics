70ec23dec434ed4f16a4c15b2caa378cf6e512a6
Ethan Baik <sakithree@gmail.com>
2023-02-20 14:11:49 +0900

    hough lines

--- ch09.cpp
+++ ch09.cpp
@@ -7,11 +7,13 @@ using namespace cv;
 /// Function Declarations
 void sobel_edge();
 void canny_edge();
+void hough_lines();
 
 int main(void)
 {
     // sobel_edge();
-    canny_edge();
+    // canny_edge();
+    hough_lines();
 
     waitKey();
     destroyAllWindows();
@@ -62,4 +64,40 @@ void canny_edge()
     imshow("src", src);
     imshow("dst1", dst1);
     imshow("dst2", dst2);
+}
+
+void hough_lines()
+{
+    Mat src = imread("img/building.jpg", IMREAD_GRAYSCALE);
+
+    if (src.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return;
+    }
+
+    Mat edge;
+    Canny(src, edge, 50, 150);
+
+    vector<Vec2f> lines;
+    // use canny_edge image, pixel resolution = 1, degree = 1, threshold 250
+    HoughLines(edge, lines, 1, CV_PI / 180, 250);
+
+    Mat dst;
+    cvtColor(edge, dst, COLOR_GRAY2BGR);
+
+    for (size_t i = 0; i < lines.size(); i++)
+    {
+        float r = lines[i][0];  // rho for each line
+        float t = lines[i][1];  // theta for each line
+        double cos_t = cos(t), sin_t = sin(t);  // calc cos(t) & sin(t)
+        double x0 = r * cos_t, y0 = r * sin_t;  // calc x0, y0 from rho, theta
+        double alpha = 1000;    // offset to calculate 2 points from single pt
+        Point pt1(cvRound(x0 + alpha * (-sin_t)), cvRound(y0 + alpha * cos_t));
+        Point pt2(cvRound(x0 - alpha * (-sin_t)), cvRound(y0 - alpha * cos_t));
+        line(dst, pt1, pt2, Scalar(0, 0, 255), 1, LINE_AA);
+    }
+
+    imshow("src", src);
+    imshow("dst", dst);
 }
