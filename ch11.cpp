896cdb7b402d41213de835fb56fa0a690f8874f8
Ethan Baik <sakithree@gmail.com>
2023-02-24 15:15:07 +0900

    Adaptive threshold

--- ch11.cpp
+++ ch11.cpp
@@ -8,11 +8,14 @@ using namespace cv;
 void threshold_trackbar();
 void on_threshold(int pos, void *userdata);
 void threshold_otsu();
+void threshold_adaptive();
+void on_trackbar_adapt(int pos, void *userdata);
 
 int main(void)
 {
     // threshold_trackbar();
-    threshold_otsu();
+    // threshold_otsu();
+    threshold_adaptive();
 
     destroyAllWindows();
 
@@ -67,4 +70,38 @@ void threshold_otsu()
     imshow("dst", dst);
 
     waitKey();
+}
+
+void threshold_adaptive()
+{
+    Mat src = imread("img/sudoku.jpg", IMREAD_GRAYSCALE);
+
+    if (src.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return;
+    }
+
+    namedWindow("sudoku");
+    int val = 0;
+    createTrackbar("Block Size", "sudoku", &val, 200, on_trackbar_adapt, &src);
+    setTrackbarPos("Block Size", "sudoku", 3);
+
+    waitKey();
+}
+
+void on_trackbar_adapt(int pos, void *userdata)
+{
+    Mat src = *(Mat *)userdata;
+
+    // set min value to 3 & force odd values
+    if (pos % 2 == 0)
+        pos--;
+    if (pos < 3)
+        pos = 3;
+
+    Mat dst;
+    adaptiveThreshold(src, dst, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, pos, 5);
+
+    imshow("sudoku", dst);
 }
